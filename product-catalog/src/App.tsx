548d1ec748daa27f120d99426e06a77371eba042
Jashan Devgan <ashleshadhanjal@gmail.com>
2023-08-02 01:58:05 +0530

    Add components folder

--- product-catalog/src/App.tsx
+++ product-catalog/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { SearchScreen } from './screens';
+import { SearchBar } from './components';
 function App() {
   return (
     <div className="App">
       <SearchScreen />
+      <SearchBar />
     </div>
   );
 }
